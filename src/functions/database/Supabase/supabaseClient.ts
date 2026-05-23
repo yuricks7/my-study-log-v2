fe25a2b38df777b0d24c8379d3e01c8b0f580080
20250824_yuricks7 <yuricks7+mousePC@dekoboko-hack.biz>
2026-05-23 18:31:45 +0900

    changed the table name.

--- src/functions/database/Supabase/supabaseClient.ts
+++ src/functions/database/Supabase/supabaseClient.ts
@@ -7,12 +7,4 @@ export const supabaseUrl: any
 export const supabasePublishableKey: any
       = process.env.VITE_SUPABASE_PUBLISHABLE_API_KEY;
 
-// export const supabaseUrl: any
-//       = import.meta.env.VITE_SUPABASE_URL;
-// export const supabasePublishableKey: any
-//       = import.meta.env.VITE_SUPABASE_PUBLISHABLE_API_KEY;
-
-console.log(supabaseUrl);
-console.log(supabasePublishableKey);
-
 export const supabase: any = createClient(supabaseUrl, supabasePublishableKey);
