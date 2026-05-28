9a12e90fd2487e6ed4b10ca6999b3b9eed4f6cf4
20250824_yuricks7 <yuricks7+mousePC@dekoboko-hack.biz>
2026-05-28 18:26:17 +0900

    deleted the unnecessary comments.

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,6 @@ import { HistoryArea } from "./components/organisms/HistoryArea"
 
 import type { RecordType } from './@types/RecordType';
 
-import { useContext } from "react";
-import { ErrorContext, ErrorProvider } from "./providers/ErrorProvider";
-import { hasError } from './functions/input/hasError';
-
 export default function App() {
   const [records, setRecords] = useState<RecordType[]>([]);
   const [sum, setSum] = useState<number>(0);
@@ -34,6 +30,54 @@ export default function App() {
     load();
   }, []);
 
+  const handleAdd = async (title: string, time: number) => {
+    // バリデーション
+    if (isInvalidInput(title, time)) return;
+    if (!canContinue(title, time, "追加")) return;
+
+    // データを追加
+    const newRecord: RecordType = await DbUsecase.add(title, time);
+    const newList: RecordType[] = [...records, newRecord];
+    setRecords(newList);
+    setSum(updateSumTime(newList));
+
+    // 初期化
+    setTitle("");
+    setTime(0);
+  };
+
+  const handleUpdate = async (id: string, title: string, time: number) => {
+    // バリデーション
+    if (isInvalidInput(title, time)) return;
+    if (!canContinue(title, time, "上書き")) return;
+
+    // 更新
+    const updated: RecordType = await DbUsecase.update(id, title, time);
+    const newList: RecordType[] = records.map((row) =>
+      row.id === id ? updated : row
+    );
+
+    setRecords(newList);
+    setSum(updateSumTime(newList));
+
+    // 初期化
+    setTitle("");
+    setTime(0);
+  };
+
+  const handleDelete = async (id: string) => {
+    await DbUsecase.remove(id);
+
+    const newList: RecordType[] = records.filter(
+      (row) => row.id !== id
+    );
+    setRecords(newList);
+    setSum(updateSumTime(newList));
+  };
+
+  // =====================================
+  // 関数の定義
+  // =====================================
   const isInvalidInput = (title: string, time: number): boolean => {
     if (title === "" && time <= 0) {
       setHasTitleError(true);
@@ -60,83 +104,12 @@ export default function App() {
     let m: string = '';
     m += `この内容で${action}しますか？\n`;
     m += `内容：${title}\n`;
-    m += `時間：${time}`;
+    m += `時間：${time}時間`;
     if (!confirm(m)) return false;
 
     return true;
   }
 
-  /**
-   * 追加
-   *
-   * @param title
-   * @param time
-   */
-  const handleAdd = async (title: string, time: number) => {
-    // バリデーション
-    if (isInvalidInput(title, time)) return;
-    if (!canContinue(title, time, "追加")) return;
-
-    // データを追加
-    const newRecord: RecordType = await DbUsecase.add(title, time);
-    const newList: RecordType[] = [...records, newRecord];
-    setRecords(newList);
-    setSum(updateSumTime(newList));
-
-    // 初期化
-    setTitle("");
-    setTime(0);
-  };
-
-  /**
-   * 更新
-   *
-   * @param id
-   * @param title
-   * @param time
-   */
-  const handleUpdate = async (id: string, title: string, time: number) => {
-    // バリデーション
-    if (isInvalidInput(title, time)) return;
-    if (!canContinue(title, time, "上書き")) return;
-
-    // 更新
-    const updated: RecordType = await DbUsecase.update(id, title, time);
-    const newList: RecordType[] = records.map((row) =>
-      row.id === id ? updated : row
-    );
-
-    setRecords(newList);
-    setSum(updateSumTime(newList));
-
-    // 初期化
-    setTitle("");
-    setTime(0);
-  };
-
-  /**
-   * 削除
-   *
-   * @param id
-   */
-  const handleDelete = async (id: string) => {
-    await DbUsecase.remove(id);
-
-    const newList: RecordType[] = records.filter(
-      (row) => row.id !== id
-    );
-    setRecords(newList);
-    setSum(updateSumTime(newList));
-  };
-
-  // =====================================
-  // 関数の定義
-  // =====================================
-  /**
-   * 合計時間を算出する
-   * @param arr
-   * @returns 合計時間
-   */
   // @ts-ignore Target signature provides too few arguments. Expected 1 or more, but got 0.
   const updateSumTime = (arr: RecordType[]): number => {
     let ret: number = 0;
@@ -151,29 +124,27 @@ export default function App() {
   // テンプレート
   // =====================================
   return (
-    <ErrorProvider>
-      <SContainer>
-        <div className='container'>
-          <h1>学習記録アプリ</h1>
-          <FormArea
-            title={title} setTitle={setTitle}
-            time={time} setTime={setTime}
-            records={records} setRecords={setRecords}
-            sum={sum} setSum={setSum}
-            updateSumTime={updateSumTime}
-            hasTitleError={hasTitleError}
-            hasTimeError={hasTimeError}
-            handleAdd={handleAdd}
-          />
-          <HistoryArea
-            title={title} time={time}
-            sum={sum} records={records}
-            handleUpdate={handleUpdate}
-            handleDelete={handleDelete}
-          />
-        </div>
-      </SContainer>
-    </ErrorProvider>
+    <SContainer>
+      <div className='container'>
+        <h1>学習記録アプリ</h1>
+        <FormArea
+          title={title} setTitle={setTitle}
+          time={time} setTime={setTime}
+          records={records} setRecords={setRecords}
+          sum={sum} setSum={setSum}
+          updateSumTime={updateSumTime}
+          hasTitleError={hasTitleError}
+          hasTimeError={hasTimeError}
+          handleAdd={handleAdd}
+        />
+        <HistoryArea
+          title={title} time={time}
+          sum={sum} records={records}
+          handleUpdate={handleUpdate}
+          handleDelete={handleDelete}
+        />
+      </div>
+    </SContainer>
   );
 }
 
